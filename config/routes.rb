9573f87521438197fe278c80b55b04246537a78e
Rayan Castro <rayancdc@gmail.com>
2019-05-21 18:05:42 -0300

    Add pet show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pets', to: 'pets#index'
-
+  get 'pets/:id', to: 'pets#show', as: :pet
 end
