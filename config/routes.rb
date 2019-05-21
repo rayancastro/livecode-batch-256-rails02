f738a2b11a1c7d4a755bc1f854966fbd1b9c3a3f
Rayan Castro <rayancdc@gmail.com>
2019-05-21 18:39:45 -0300

    Add Pet CRUD routes and actions, bootstrap and simple form install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pets/new', to: "pets#new", as: :new_pet
+  post 'pets', to: "pets#create"
+
+  get 'pets/:id/edit', to: "pets#edit", as: :edit_pet
+  patch 'pets/:id', to: "pets#update"
+
+  delete 'pets/:id', to: "pets#destroy"
+
   get 'pets', to: 'pets#index'
   get 'pets/:id', to: 'pets#show', as: :pet
 end
